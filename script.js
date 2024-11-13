8faaa0151cf0a2d4756cb2a7156c61c89c0d31e1
Theblad1 <subir.singh8@icloud.com>
2024-11-13 20:21:57 +0800

    JS Function

---
+++ script.js
@@ -0,0 +1,22 @@
+function myfunction() {
+    document.write("Button clicked")
+}
+
+function makeSquare(num = 8) {
+    document.write(num * num)
+}
+
+var add = function (a, b) {
+    document.write(a + b);
+}
+
+var num1 = 20;
+
+function multiply() {
+    var num2 = 30;
+    return num1 * num2
+}
+
+var result = multiply();
+document.write(num2)
+
